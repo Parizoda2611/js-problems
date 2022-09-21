79735e38dc495702b36901c95731bbbb47aa5bca
Parizoda2611 <parizodarakhmankulova@gmail.com>
2022-09-21 14:49:56 +0500

    main

---
+++ main.js
@@ -0,0 +1,283 @@
+// Data Types
+
+// N1
+
+/* const c = prompt()
+
+let m = 100
+
+if(c / m){
+  console.log(c / m)
+} */
+
+// N2
+
+/* const M = prompt()
+
+let t = 1000
+
+if(M / t){
+  console.log(M / t);
+} */
+
+// N3
+
+/* const A = prompt()
+const B = prompt()
+
+if(A > B){
+  console.log(A / B);
+}else{
+  console.log("False")
+} */
+
+// N4
+
+/* const number = prompt()
+let number2 = 10
+
+if(number/10){
+ console.log(Math.floor(number/10));
+ console.log(number%10);
+} */
+
+// N5
+
+/* const number = prompt()
+let number2 = 10
+let a = number/number2
+let b = number%number2
+
+if(a + b){
+  console.log(Math.floor(a + b));
+} */
+
+// N6
+
+/* const number = prompt()
+let number2 = 10
+let a = number/number2
+let b = number%number2
+
+if(a * b){
+  console.log(Math.floor(a)*(b));
+} */
+
+// N7
+
+/* const a = prompt()
+let b = a/1000
+let c = a-b*1000
+let d = c/100
+
+if(c/100){
+  console.log(d = c/100);
+} */
+
+// N8
+
+/* const a = prompt()
+let b = a/60
+let c = a/3600
+
+if(a, b){
+  console.log(a/60);
+  console.log(a/3600)
+} */
+
+// For
+
+// N1
+/* let s = prompt("Hafta kunini kiriting: ") - 0;
+switch (s) {
+  case 1:
+    console.log("Dushanba");
+    break;
+  case 2:
+    console.log("Seshanba");
+    break;
+    case 3:
+      console.log("Chorshanba");
+      break;
+      case 4:
+        console.log("Payshanba");
+        break;
+        case 5:
+          console.log("Juma");
+          break;
+          case 6:
+            console.log("Shanba");
+            break;
+            case 7:
+              console.log("Yakshanba");
+              break;
+  default:
+    break;
+} */
+
+// N2
+
+/* let k = 2
+switch (k) {
+  case 1:
+    console.log("Yomon");
+    break;
+  case 2:
+    console.log("qoniqarsiz");
+    break;
+    case 3:
+      console.log("Qoniqarli");
+      break;
+      case 4:
+        console.log("Yaxshi");
+        break;
+        case 5:
+          console.log("A'lo");
+          break;
+  default:
+    break;
+} */
+
+// N3
+
+/* let m = 3
+switch (m) {
+  case 1:
+    console.log("Qish");
+    break;
+    case 2:
+      console.log("Qish");
+      case 3:
+        console.log("Bahor");
+        break;
+        case 4:
+        console.log("Bahor");
+        break;
+        case 5:
+        console.log("Bahor");
+        break;
+        case 6:
+          console.log("Yoz");
+          break;
+          case 7:
+          console.log("Yoz");
+          break;
+          case 8:
+          console.log("Yoz");
+          break;
+          case 9:
+            console.log("Kuz");
+            break;
+            case 10:
+            console.log("Kuz");
+            break;
+            case 11:
+            console.log("Kuz");
+            break;
+            case 12:
+              console.log("Qish");
+              break;
+  default:
+    break;
+} */
+
+// N4
+
+/* let m = 3
+switch (m) {
+  case 1:
+    console.log("31")
+    break;
+  case 2:
+    console.log("28");
+    break;
+    case 3:
+      console.log("31");
+      break;
+      case 4:
+      console.log("30");
+      break;
+      case 5:
+        console.log("31");
+        break;
+        case 6:
+          console.log("30");
+          break;
+          case 7:
+            console.log("31");
+            break;
+            case 8:
+              console.log("31");
+              break;
+              case 9:
+                console.log("30");
+                break;
+                case 10:
+                  console.log("31");
+                  break;
+                  case 11:
+                    console.log("30");
+                    break;
+                    case 12:
+                      console.log("31");
+                      break;
+  default:
+    console.log("Bunday oy yo'q")
+    break;
+} */
+
+// N5
+
+/* let a = 3, b = 4;
+let c = prompt()
+
+switch (c) {
+  case "+":
+    console.log(a + b)
+    break;
+case "-":
+console.log(a - b);
+break;
+case "*":
+console.log(a * b);
+break;
+case "/":
+console.log(a / b);
+break;
+  default:
+    console.log("Unaqa amal yo'q");
+    break;
+} */
+
+// N6
+
+/* let score = 70;
+
+switch (score) {
+  case 120 > score > 100:
+    console.log("400 000 so'm");
+    break;
+  case 100 > score > 90:
+    console.log("300 000 so'm");
+    break;
+    case 90 > score > 70:
+    console.log("200 000 so'm");
+    break;
+    case 70 > score > 50:
+      console.log("100 000 so'm");
+      break;
+      case 50 > score:
+        console.log("0 so'm");
+        break;
+  default:
+    break;
+} */
+
+ 
+
+
+
+
+
+
+
